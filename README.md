1abdc7c3dab33017c88df26ef1dc7c4268cc6cf9
slievehooksey <js834hww8dh23bsksd@outlook.com>
2024-07-10 17:17:09 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,78 @@
+# Bluetooth Serial Port to MQTT Bridge
+This project is intended to be used in conjunction with --Ambrogio Mower Controller (link coming soon)-- in order to allow control of AM4000 based mowers via WiFi (and Home Assistant). It connects to your mower via Bluetooth and relays serial data to an MQTT broker.
+
+# Disclaimer
+Any useage of this project's software and hardware are at your own risk. This includes any damage done to you or your equipment as well as the loss of warranty when the mower is opened by a non-authorized person.
+
+## Quickstart
+
+Flash the latest release to an original ESP32 module (S3, C3 etc. do not support Bluetooth Classic). The module will then start as a WiFi access point with the network name (SSID) starting ESP_XXXXXX.
+
+Connect to this WiFi network and you will be directed to a page to enter your network and MQTT broker details:
+
+<img src="./img/Settings.png">
+
+| Setting | Description |
+| - | :- |
+| Thing Name | The name of your mower. This will for part of the MQTT topic and should be unique |
+| AP password | This is used to protect the WiFi network that is advertised in fallback AP mode, and to access the firmware update page |
+| WiFi SSID | The name of your WiFi (this must be typed in as the library used does not have scanning functionality) |
+| WiFi Password | The password to the above WiFi network |
+| MQTT Server | The IP address of the MQTT broker |
+| MQTT User | The username to connect to the broker |
+| MQTT Password | The password of the above user |
+| MQTT Topic Prefix | The base topic to use, the default of ambrogio is usually appropriate |
+| Mower MAC Address | Unless you already know the Bluetooth MAC address of your mower, leave this blank and the bridge will scan for it |
+| Connect to Mower | Controls whether the bridge should connect to the mower. As only one BT connection is supported at a time, this is used to allow connection from the original App |
+
+Hit apply and the bridge will reboot and connect to the WiFi network and MQTT broker specified. At this point you should be able to find the IP address that has been assigned to your bridge from your router or it will be displayed in the serial port monitor. Any problems (incorrect passwords etc.) will also be displayed on the serial monitor.
+
+Once connected successfully, browse to the bridge via IP.
+
+<img src="./img/Scanning.png">
+
+Keep refreshing the page until the mower is found. The mower needs to be relatively close for it to be discovered (up tp around 10-15 metres). Copy the MAC address of your mower, go to the 'configure page', then enter it in the MAC address box and apply.
+
+The bridge will reboot and start trying to connect to your mower. If any of the connections drop (WiFi/MQTT/Bluetooth), the bridge will indefinitely try and re-establish them. 
+
+At this point you should be able to subscribe to the 'serialout' topic using an MQTT client and view the data the mower is outputting. The base topic is shown on the home page (ambrogio/AmbrogioBTSerialBridge in the above example), so just add /serialout to get the full topic. i.e ambrogio/AmbrogioBTSerialBridge/serialout
+
+<img src="./img/MQTT.png">
+
+That's it! The next step is to build a controller that can process the serial output and send commands to the mower. (Instructions coming soon)
+
+## Module Placement
+
+Once connected, the bluetooth range is considerably better than during discovery. Distances up to about 40m line-of-sight have been stable, so depending on your lawn layout and dimensions, you may simply need to position the bridge appropriately. If the connection is dropped however, it will struggle to re-establish until the mower is withing about 20m.
+
+If your lawn is bigger than this, you could try multiple bridges configured with the same thing name, and therefore the same topic. This has not been tested, however.
+
+Another option if you have full WiFi coverage of your lawn is to place the module inside the mower. The programming header has GND and 5V pins (the right-most pins in this image). This has not been tested, however.
+
+
+## Background
+Ambrogio manufacture several models of mower (I have a Twenty Elite S+) based on the AM4000 control board. These mowers can be configured/controlled via App which can communicate in one of two ways:
+- Via Bluetooth, allows full configuration and control of the mower but is quite limited in range (up to about 10m).
+- Via a 'Connect' module which is pre-installed in some models or can be added as an extra on others. This uses cellular data and requires a subscription. The connect module also provides GPS functionality. The control options are also very limited using this method and updates can be slow.
+
+I wanted to be able to monitor and control my mower using homeassistant, and there is a great integration that does this via the ZCS cloud and Connect module (https://github.com/ufozone/ha-zcs-mower), but this comes with the limitations mentioned above.
+
+There is also the [Robotan project](https://github.com/fredlcore/Robotan) that interfaces with the Serial port on older models, which unfortunately does not exist on the AM4000 (See note [here](https://github.com/fredlcore/Robotan/blob/master/Supported%20Models.md)). The author of that project did give me a jump start on decoding the protocol, which was immensely helpful for the other half of the project, the controller.
+
+## Bluetooth Serial
+Although there is no convenient serial port like on previous models, I was hopeful the serial data would be available on a header or test pads somewhere on the board. The AM4000 uses a BT121-A bluetooth chip for communication, which according to the data sheet has the Serial Port on pins 26-29
+
+<IMG SRC="./img/BTChip.jpeg">
+<IMG SRC="./img/BTChipCloseUp.jpeg">  
+<img src="./img/BTChipPinout.png">
+<img src="./img/BTChipPinAssignments.png">
+
+I couldn't find anywhere on the board that these lines were exposed conveniently, but I was able to tap into them directly from the chip and read the serial data, but whenever I tried to transmit, it locked up the mower and it needed to be restarted. My electronics skills and willingness to potentially brick my mower ended there so I looked at connecting as the App does, via Bluetooth.
+
+Connections from the App are made via Bluetooth Classic using Serial Port Profile (SPP). Handily, an ESP32 (original, S3, C3 etc. only support BLE) has support for SPP through the [BluetoothSerial](https://github.com/espressif/arduino-esp32/tree/master/libraries/BluetoothSerial) library.
+
+# FAQ
+
+### Why not have the controller and bridge on the same device?
+- The Arduino Bluetooth stack takes up a LOT of the available memory on the ESP32. Adding the controller functionality on the same module was causing instability
+
---
+++ src/main.cpp
@@ -0,0 +1,353 @@
+#include <Arduino.h>
+#include <MQTT.h>
+#include <IotWebConf.h>
+#include <IotWebConfUsing.h> // This loads aliases for easier class names.
+#include <IotWebConfESP32HTTPUpdateServer.h>
+#include <BluetoothSerial.h>
+#include <Regexp.h>
+
+
+// -- Initial name of the Thing. Used e.g. as SSID of the own Access Point.
+const char thingName[] = "AmbrogioBTSerialBridge";
+
+// -- Initial password to connect to the Thing, when it creates an own Access Point.
+const char wifiInitialApPassword[] = "letmein";
+
+#define STRING_LEN 128
+
+// -- Configuration specific key. The value should be modified if config structure was changed.
+#define CONFIG_VERSION "mqt1"
+
+// -- Status indicator pin.
+//      First it will light up (kept LOW), on Wifi connection it will blink,
+//      when connected to the Wifi it will turn off (kept HIGH).
+#define STATUS_PIN LED_BUILTIN
+
+// -- Method declarations.
+void handleRoot();
+void mqttMessageReceived(MQTTClient *client, char topic[], char bytes[], int length);
+bool connectMqtt();
+bool connectMqttOptions();
+// -- Callback methods.
+void wifiConnected();
+void configSaved();
+bool formValidator(iotwebconf::WebRequestWrapper* webRequestWrapper);
+
+void printMsg(char* msg, uint8_t len, uint8_t offset1 = 0, uint8_t offset2 = 0);
+
+// -- Bluetooth callbacks
+void btDataAvailable(const uint8_t *buffer, size_t size);
+void writeToBluetoothSerial(const uint8_t *buffer, size_t size);
+void handleBtDeviceFound(BTAdvertisedDevice *device);
+
+DNSServer dnsServer;
+WebServer server(80);
+HTTPUpdateServer httpUpdater;
+WiFiClient net;
+MQTTClient mqttClient(256); //128 is default but payloads can be up to 256
+BluetoothSerial SerialBT;
+
+char mqttServerValue[STRING_LEN];
+char mqttUserNameValue[STRING_LEN];
+char mqttUserPasswordValue[STRING_LEN];
+char mqttPrefixValue[STRING_LEN];
+
+char mowerMacAddressValue[18];
+char mowerConnectValue[STRING_LEN];
+
+String mqttTopic;
+String mqttTopicSerialOut;
+String mqttTopicSerialIn;
+
+uint8_t mowerAddress[6]; //replace with entry from config
+bool btConnected = false; 
+long lastBTConnectAttempt; //store the last time a BT connection attempt was made. Used to avoid retrying too often.
+String scanOutput = "No scan output. If there is a valid MAC address set, scanning will not occur."; //placeholder for Bluetooth scann output
+
+IotWebConf iotWebConf(thingName, &dnsServer, &server, wifiInitialApPassword, CONFIG_VERSION);
+
+IotWebConfParameterGroup mqttGroup = IotWebConfParameterGroup("mqtt", "MQTT configuration");
+IotWebConfTextParameter mqttServerParam = IotWebConfTextParameter("MQTT server", "mqttServer", mqttServerValue, STRING_LEN);
+IotWebConfTextParameter mqttUserNameParam = IotWebConfTextParameter("MQTT user", "mqttUser", mqttUserNameValue, STRING_LEN);
+IotWebConfPasswordParameter mqttUserPasswordParam = IotWebConfPasswordParameter("MQTT password", "mqttPass", mqttUserPasswordValue, STRING_LEN);
+IotWebConfTextParameter mqttPrefixParam = IotWebConfTextParameter("MQTT topic prefix (default 'ambrogio')", "mqttPrefix", mqttPrefixValue, STRING_LEN, "ambrogio");
+
+IotWebConfParameterGroup mowerGroup = IotWebConfParameterGroup("mower", "Mower configuration");
+IotWebConfTextParameter mowerMacAddressParam = IotWebConfTextParameter("Mower MAC Address", "mowerMac", mowerMacAddressValue, 18);
+IotWebConfCheckboxParameter mowerConnectParam = IotWebConfCheckboxParameter("Connect to Mower?", "mowerConnect", mowerConnectValue, STRING_LEN, true);
+
+bool needMqttConnect = false;
+bool needReset = false;
+int pinState = HIGH;
+unsigned long lastReport = 0;
+unsigned long lastMqttConnectionAttempt = 0;
+unsigned long lastSentToBt = 0;
+
+void setup() 
+{
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("Starting up...");
+
+  mqttGroup.addItem(&mqttServerParam);
+  mqttGroup.addItem(&mqttUserNameParam);
+  mqttGroup.addItem(&mqttUserPasswordParam);
+  mqttGroup.addItem(&mqttPrefixParam);
+
+  mowerGroup.addItem(&mowerMacAddressParam);
+  mowerGroup.addItem(&mowerConnectParam);
+
+  iotWebConf.setStatusPin(STATUS_PIN);
+  iotWebConf.addParameterGroup(&mqttGroup);
+  iotWebConf.addParameterGroup(&mowerGroup);
+  iotWebConf.setConfigSavedCallback(&configSaved);
+  iotWebConf.setFormValidator(&formValidator);
+  iotWebConf.setWifiConnectionCallback(&wifiConnected);
+  iotWebConf.skipApStartup();
+
+  // -- Initializing the configuration.
+  bool validConfig = iotWebConf.init();
+  if (!validConfig)
+  {
+    mqttServerValue[0] = '\0';
+    mqttUserNameValue[0] = '\0';
+    mqttUserPasswordValue[0] = '\0';
+  }
+
+  mqttTopic = String(mqttPrefixValue) + "/" + String(iotWebConf.getThingName());
+  mqttTopicSerialIn = mqttTopic + "/serialin";
+  mqttTopicSerialOut = mqttTopic + "/serialout";
+
+   // -- Define how to handle updateServer calls.
+  iotWebConf.setupUpdateServer(
+    [](const char* updatePath) { httpUpdater.setup(&server, updatePath); },
+    [](const char* userName, char* password) { httpUpdater.updateCredentials(userName, password); });
+
+  // -- Set up required URL handlers on the web server.
+  server.on("/", handleRoot);
+  server.on("/config", []{ iotWebConf.handleConfig(); });
+  server.onNotFound([](){ iotWebConf.handleNotFound(); });
+
+  mqttClient.setKeepAlive(30000);//the default of 10000 is too short when the bluetooth connect is also 10 seconds
+  mqttClient.begin(mqttServerValue, net);
+  mqttClient.onMessageAdvanced(mqttMessageReceived);
+
+  SerialBT.onData(btDataAvailable);
+  SerialBT.begin("Ambrogio BTSerial Bridge", true);
+
+  Serial.println("Ready.");
+}
+
+void loop() 
+{
+  // -- doLoop should be called as frequently as possible.
+  iotWebConf.doLoop();
+  mqttClient.loop();
+  
+  if (needMqttConnect)
+  {
+    if (connectMqtt())
+    {
+      needMqttConnect = false;
+    }
+  }
+  else if ((iotWebConf.getState() == iotwebconf::OnLine) && (!mqttClient .connected()))
+  {
+    Serial.println("MQTT reconnect");
+    connectMqtt();
+  }
+
+  if (needReset)
+  {
+    Serial.println("Rebooting after 1 second.");
+    iotWebConf.delay(1000);
+    ESP.restart();
+  }
+
+  if(btConnected && !SerialBT.connected()){ //BT Serial connection has become disconnected
+  Serial.println("BT Serial connection closed");
+  btConnected = SerialBT.connected();
+  }
+
+if(mowerConnectParam.isChecked() && !btConnected && mqttClient.connected() && (lastBTConnectAttempt + 30000 < millis() || lastBTConnectAttempt == 0)){ //if we are wanting to be connected, and aren't already
+  
+      MatchState ms;
+      ms.Target(mowerMacAddressValue);
+      char result = ms.Match("^[0-9A-Fa-f][0-9A-Fa-f]:[0-9A-Fa-f][0-9A-Fa-f]:[0-9A-Fa-f][0-9A-Fa-f]:[0-9A-Fa-f][0-9A-Fa-f]:[0-9A-Fa-f][0-9A-Fa-f]:[0-9A-Fa-f][0-9A-Fa-f]$");
+    if (result == REGEXP_MATCHED)
+    { 
+      Serial.print("Attempting to connect to mower by MAC: ");
+      Serial.println(mowerMacAddressValue);
+
+      sscanf(mowerMacAddressValue, "%2hhx:%2hhx:%2hhx:%2hhx:%2hhx:%2hhx", &mowerAddress[0], &mowerAddress[1], &mowerAddress[2], &mowerAddress[3], &mowerAddress[4], &mowerAddress[5]);
+        char* ptr; //start and end pointer for strtol
+
+        mowerAddress[0] = strtol(mowerMacAddressValue, &ptr, HEX );
+        for( uint8_t i = 1; i < 6; i++ )
+        {
+          mowerAddress[i] = strtol(ptr+1, &ptr, HEX );
+        }
+      SerialBT.connect(mowerAddress);
+      lastBTConnectAttempt = millis();
+    }
+    else if(result == REGEXP_NOMATCH){
+      Serial.println("Not a valid MAC address");
+      scanOutput = "Scanning....<br><br>";
+      SerialBT.discoverAsync(handleBtDeviceFound, 25000);
+      lastBTConnectAttempt = millis();
+    }
+    else{
+      Serial.println("Regex parsing error");
+    }
+  
+  if(SerialBT.connected()){//connection has been made successfully
+    Serial.println("Connected Succesfully!");
+    btConnected = SerialBT.connected();
+  }
+}
+else if(!mowerConnectParam.isChecked() && btConnected){//we are connected but should be disconnected
+    SerialBT.disconnect();
+}
+  
+}
+
+/**
+ * Handle web requests to "/" path.
+ */
+void handleRoot()
+{
+  // -- Let IotWebConf test and handle captive portal requests.
+  if (iotWebConf.handleCaptivePortal())
+  {
+    // -- Captive portal request were already served.
+    return;
+  }
+  String s = "<!DOCTYPE html><html lang=\"en\"><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no\"/>";
+  s += "<title>" + String(thingName) + "</title></head><body>Bluetooth Serial Port to MQTT Bridge";
+  s += "<ul>";
+  s += "<li>MQTT server: ";
+  s += mqttServerValue;
+  s += "</li>";
+  s += "<li>MQTT Topic: ";
+  s += mqttTopic;
+  s += "</li>";
+  s += "</ul>";
+  s += "Go to <a href='config'>configure page</a> to change values.";
+  s += "<br><br><h3>Bluetooth Scan Output:</h3>";
+  s += scanOutput;
+  s += "</body></html>\n";
+
+  server.send(200, "text/html", s);
+}
+
+void wifiConnected()
+{
+  needMqttConnect = true;
+}
+
+void configSaved()
+{
+  Serial.println("Configuration was updated.");
+  needReset = true;
+}
+
+bool formValidator(iotwebconf::WebRequestWrapper* webRequestWrapper)
+{
+  Serial.println("Validating form.");
+  bool valid = true;
+
+  int l = webRequestWrapper->arg(mqttServerParam.getId()).length();
+  if (l < 3)
+  {
+    mqttServerParam.errorMessage = "Please provide at least 3 characters!";
+    valid = false;
+  }
+
+  return valid;
+}
+
+bool connectMqtt() {
+  unsigned long now = millis();
+  if (1000 > now - lastMqttConnectionAttempt)
+  {
+    // Do not repeat within 1 sec.
+    return false;
+  }
+  Serial.println("Connecting to MQTT server...");
+  if (!connectMqttOptions()) {
+    lastMqttConnectionAttempt = now;
+    return false;
+  }
+  Serial.println("Connected!");
+
+  mqttClient.subscribe(mqttTopicSerialIn);
+  return true;
+}
+
+bool connectMqttOptions()
+{
+  bool result;
+  if (mqttUserPasswordValue[0] != '\0')
+  {
+    result = mqttClient.connect(iotWebConf.getThingName(), mqttUserNameValue, mqttUserPasswordValue);
+  }
+  else if (mqttUserNameValue[0] != '\0')
+  {
+    result = mqttClient.connect(iotWebConf.getThingName(), mqttUserNameValue);
+  }
+  else
+  {
+    result = mqttClient.connect(iotWebConf.getThingName());
+  }
+  return result;
+}
+
+void mqttMessageReceived(MQTTClient *client, char topic[], char bytes[], int length)
+{
+  //should only ever get messages on this topic as that's all that is subscribed, but check anyway
+  if(strcmp(topic, mqttTopicSerialIn.c_str()) == 0)
+  {
+    uint8_t data[length];
+    memcpy(data,bytes,length);
+    SerialBT.write(data,length);
+    }
+}
+
+void btDataAvailable(const uint8_t *buffer, size_t size){
+  char message[size];
+  memcpy(message,buffer,size);
+  int msgPos = 0;
+  int frameStart = 0;
+  //sometimes 2 frames of data can come in together, so split them up
+  for(size_t i = 0; i < size; i++ ){
+    if(buffer[i] == 0x03){
+      char packet[i - frameStart + 1];
+      memcpy(packet,buffer+frameStart,sizeof(packet));
+      mqttClient.publish(mqttTopicSerialOut.c_str(),packet,sizeof(packet));
+      frameStart = i + 1;
+    }
+  } 
+}
+
+void writeToBluetoothSerial(const uint8_t *buffer, size_t size){
+  if(SerialBT.connected()){
+  SerialBT.write(buffer,size);
+  lastSentToBt = millis();
+  }
+}
+
+void printMsg(char* msg, uint8_t len, uint8_t offset1, uint8_t offset2) {
+  for (int z=offset1; z<len-offset2; z++) {
+    if (msg[z] < 16) Serial.print("0");
+    Serial.print((char)msg[z], HEX);
+    Serial.print (" ");
+  }
+  Serial.println();
+}
+
+void handleBtDeviceFound(BTAdvertisedDevice *device){
+      scanOutput += "<br>";
+      scanOutput += device->getAddress().toString(true);
+      scanOutput += " - ";
+      scanOutput += device->getName().c_str();
+}
